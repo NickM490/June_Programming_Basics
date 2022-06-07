7b3f1d2a67bce99be6445845e3f1f7d04bf29b34
NickM490 <nick4sites@gmail.com>
2022-06-07 09:01:33 -0700

    adding day 1 code

---
+++ week1Day1/day1.js
@@ -0,0 +1,35 @@
+// Code is read from the top down
+// Variable values may change depending on the order of the code
+
+
+// Declaring a variable (x) and assigning a value (3)
+var x = 3
+// Printing the value of x to the console or terminal
+console.log("*******************************************")
+console.log("************ Test Case Line 47 test.js  ***************")
+console.log(x) // 3
+
+
+
+// Declaring a variable (y) and assigning a value ("Hello There")
+var y = "Hello There"
+console.log(y) // Hello There
+
+
+var y = 17
+console.log(y) // 17
+
+
+// Declaring a variable (x) and assigning a value (10)
+var x = 10
+console.log(x) // 10
+
+
+var z = 5
+// Adding 3 + 5 and assigning the result to a variable (sum 
+var sum = x + z
+
+
+// Console Log?
+console.log("Hello World")
+
---
+++ week1Day1/test.py
@@ -0,0 +1,5 @@
+x = 5
+y = 7
+sum = x + y
+print(sum)
+print(sum*sum)
